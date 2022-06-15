70c4af65ec88db4856ca378cb301f1dfd5d552bf
O13139 <O13139@lab.aca>
2022-06-15 13:32:49 -0500

    push final con api

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,6 +2,18 @@ import React from "react";
 
 const TaskList = ()=>{
     
+    const [allProjects, setAllProjects] = React.useState([])
+
+    React.useEffect(() => {
+        async function getProjects(){
+            const res = await fetch("https://task-node-api-rest.herokuapp.com/tasks")
+            const data = await res.json()
+            setAllProjects(data)
+
+        }
+        getProjects()
+    }, [])
+
     return(
         <div className="card mt-3">
             <div className="card-header">
@@ -21,7 +33,20 @@ const TaskList = ()=>{
                          
                         </tr>
                     </thead>
-                    
+                    <tbody>
+                          
+                          {allProjects.map((project) => (
+   
+                           
+                           <tr key={project.id}>
+                               <td>{project.id}</td>
+                               <td>{project.name}</td>
+                               <td>{project.priority}</td>
+                               <td>{project.description}</td>
+                               
+                           </tr>
+                           ))}
+                       </tbody>   
                    
                        
                 </table>
